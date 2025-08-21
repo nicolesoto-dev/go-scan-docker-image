c8a898a54c2dbe7e521867304736e4b88559789c
nicole.soto0864@gmail.com <nicole.soto0864@gmail.com>
2025-08-21 17:21:31 +0200

    del

--- cmd/root.go
+++ cmd/root.go
@@ -11,8 +11,14 @@ var rootCmd = &cobra.Command{
 	Use:   "dscan",
 	Long:  "dscan is a tool that can help you to scan local and remote Docker images'",
 	Short: "Cool tool I guess",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hello World!")
+	Args:  cobra.MinimumNArgs(2),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		port, err := cmd.Flags().GetInt("port")
+		if err != nil {
+			return err
+		}
+		fmt.Printf("Serving on :%d\n", port)
+		return nil
 	},
 }
 
@@ -21,4 +27,8 @@ func Execute() {
 		fmt.Fprintf(os.Stderr, "An error while executing dscan '%s'\n", err)
 		os.Exit(1)
 	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().String("i", "", "Image to scan")
 }
